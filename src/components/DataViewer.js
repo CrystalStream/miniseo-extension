c382df10620191646b684e1d80aed67341438d25
Christofer Flores <cristofer.flort@gmail.com>
2019-09-13 18:13:53 -0500

    Add the stats cards

--- src/components/DataViewer.js
+++ src/components/DataViewer.js
@@ -5,8 +5,15 @@ import StatsCard from './StatsCard'
 export default function SeoChecker(props) {
 
   return (
-    <div>
-      <StatsCard />
+    <>
+    <div className="columns">
+      <StatsCard color="red" />
+      <StatsCard color="orange" />
     </div>
+    <div className="columns">
+      <StatsCard color="yellow "/>
+      <StatsCard color="blue" />
+    </div>
+    </>
   )
 }
--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -3,30 +3,24 @@ import React from 'react'
 export default function StatsCard(props) {
 
   return (
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title">
-          Component
-        </p>
-        <a href="#" class="card-header-icon" aria-label="more options">
-          <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-          </span>
-        </a>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-          <a href="#">@bulmaio</a>. <a href="#">#css</a> <a href="#">#responsive</a>
-          <br />
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+    <div className="column">
+      <div className={`card ${props.color}`}>
+        <header className="card-header">
+          <p className="card-header-title">
+            Component
+          </p>
+          <a href="#" className="card-header-icon" aria-label="more options">
+            <span className="icon">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </div>
         </div>
       </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Save</a>
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
     </div>
   )
 }
